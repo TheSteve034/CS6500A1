d0379dbf55ce5776779e7d5495200db98bcc940b
Samuel Reeder <swreeder88@gmail.com>
2020-01-24 23:22:43 -0700

    adjusted plotData

--- plotData.m
+++ plotData.m
@@ -13,12 +13,12 @@ function plotData(x, y)
 % Hint: You can use the 'rx' option with plot to have the markers
 %       appear as red crosses. Furthermore, you can make the
 %       markers larger by using plot(..., 'rx', 'MarkerSize', 10);
+figure; % open a new figure window
 clf;
 plot(x,y,"rx");
 title("Population Size vs Profit");
 xlabel("Population Size");
 ylabel("Profit");
-figure; % open a new figure window
 
 
 
